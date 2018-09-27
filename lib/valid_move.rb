439b4d1742fdf2f19423ef5ae522f482e336879e
David masterson <david73180@gmail.com>
2018-09-27 23:14:33 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,33 +1,7 @@
-
-
-
-def valid_move?(board, index)
-  if board == [" "," "," "," "," "," "," "," "," "]
-    return true
-  elsif board != [" "," "," "," "," "," "," "," "," "] && position_taken?(board, index) == false
-    return true
-  elsif position_taken?(board, index) == true
-    return false
-  elsif  !(board[index]).include?(0..8)
-    return false.
-  end
+# code your #valid_move? method here
+def valid_move(board, index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-
-
-
-def position_taken?(board, index)
-  case board[index]
-    when " "
-      return false
-    when ""
-      return false
-    when "X"
-      return true
-    when "O"
-      return true
-    when nil
-      return false
-  end
-end
+def position_taken(board, index)
+end 
